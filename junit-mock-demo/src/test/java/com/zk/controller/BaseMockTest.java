669b0e03e98ce9726706424ac954c51184d5601a
zhukai <zhukaixlyx@sina.com>
2020-04-23 23:47:00 +0800

    BaseMockTest代码逻辑优化

--- junit-mock-demo/src/test/java/com/zk/controller/BaseMockTest.java
+++ junit-mock-demo/src/test/java/com/zk/controller/BaseMockTest.java
@@ -13,25 +13,36 @@ import org.powermock.modules.junit4.rule.PowerMockRule;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 所有PowerMock的基类
  */
-public class BaseMockTest {
+public abstract class BaseMockTest {
 
     @Rule
-    public PowerMockRule powerMockRule = new PowerMockRule();
+    public PowerMockRule     powerMockRule = new PowerMockRule();
     @Rule
-    public ExpectedException thrown = ExpectedException.none();
+    public ExpectedException thrown        = ExpectedException.none();
 
+    /**
+     * 描述：
+     * 1、将@InjectMocks修饰的属性对象用{@Code PowerMockito.spy()}构造出来并赋值，目的是可以模拟本类的方法进行mock。
+     * 2、将@Mock修饰的属性对象用{@Code PowerMockito.mock()}构造出来并赋值，目的是mock静态方法、私有方法、final方法。
+     * 3、将所有的mock对象通过反射机制注入到spy对象中
+     * 注意：目标测试类@Mock修饰属性名称最好与目标类属性名称一致，若不一致且目标类的多个FieldType相同，则无法注入
+     */
     @Before
     public void setUp() {
         try {
-            Field[] fields = this.getClass().getDeclaredFields();
-            Object target = null;
-            Class targetType = null;
-            Map<String, MockObjInfo> mockObjInfoMap = new HashMap<>();
+            /* 给目标测试类的所有属性创建spy和mock对象，并赋值 */
+            Field[] fields = this.getClass().getDeclaredFields();// 获取测试类下的所有Field
+            Object target = null;// 目标测试类实例
+            Class targetType = null;// 目标测试类Class类型
+            Map<FieldInfo, MockObjInfo> mockObjInfoMap = new HashMap<>();// 存放所有@Mock修饰的属性名称及其对应的MockObjInfo信息
             int injectMockCount = 0;
             for (Field field : fields) {
                 field.setAccessible(true);
@@ -51,7 +62,7 @@ public class BaseMockTest {
                     field.set(this, target);
                 } else if (field.isAnnotationPresent(Mock.class)) {
                     Object mock = PowerMockito.mock(fieldType);
-                    mockObjInfoMap.put(field.getName(), new MockObjInfo(mock, fieldType));
+                    mockObjInfoMap.put(new FieldInfo(field.getName(), fieldType), new MockObjInfo(mock, fieldType));
                     field.set(this, mock);
                 }
             }
@@ -65,18 +76,37 @@ public class BaseMockTest {
                 throw new RuntimeException("@InjectMocks注释的对象只能有一个");
             }
 
-            // target赋值
             if (target == null || mockObjInfoMap.isEmpty()) {
                 return;
             }
 
+            /* 目标测试类的属性注入 */
             Field[] declaredFields = targetType.getDeclaredFields();
-            for (Field declaredField : declaredFields) {
-                declaredField.setAccessible(true);
-                MockObjInfo mockObjInfo = mockObjInfoMap.get(declaredField.getName());
-                if (mockObjInfo != null && mockObjInfo.getMockInstance() != null
-                        && declaredField.getType().isAssignableFrom(mockObjInfo.getFieldType())) {
-                    declaredField.set(target, mockObjInfo.getMockInstance());
+            for (Field targetField : declaredFields) {
+                targetField.setAccessible(true);
+
+                // 1、根据字段名称和字段类型完全一致类匹配
+                Optional<FieldInfo> optional = mockObjInfoMap.keySet().stream()
+                        .filter(fieldInfo -> targetField.getName().equals(fieldInfo.getFieldName())
+                                && targetField.getType().isAssignableFrom(fieldInfo.getFieldType()))
+                        .findFirst();
+                if (optional.isPresent()) {
+                    MockObjInfo mockObjInfo = mockObjInfoMap.get(optional.get());
+                    if (mockObjInfo != null && mockObjInfo.getMockInstance() != null) {
+                        targetField.set(target, mockObjInfo.getMockInstance());
+                        continue;
+                    }
+                }
+
+                // 2、根据字段类型类匹配
+                List<FieldInfo> collect = mockObjInfoMap.keySet().stream()
+                        .filter(fieldInfo -> targetField.getType() == fieldInfo.getFieldType())
+                        .collect(Collectors.toList());
+                if (collect != null && collect.size() == 1) {
+                    MockObjInfo mockObjInfo = mockObjInfoMap.get(collect.get(0));
+                    if (mockObjInfo != null && mockObjInfo.getMockInstance() != null) {
+                        targetField.set(target, mockObjInfo.getMockInstance());
+                    }
                 }
             }
         } catch (Exception e) {
@@ -84,11 +114,20 @@ public class BaseMockTest {
         }
     }
 
-
     @Data
     @AllArgsConstructor
     private static class MockObjInfo {
+        // 通过PowerMock.mock()创造出来的对象
         private Object mockInstance;
-        private Class fieldType;
+        // 属性类型
+        private Class  fieldType;
     }
+
+    @Data
+    @AllArgsConstructor
+    private static class FieldInfo {
+        private String fieldName;
+        private Class  fieldType;
+    }
+
 }
